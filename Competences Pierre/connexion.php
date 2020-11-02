58f54adb0785c33023207539a149a5a30b384ffb
Pierre <delattre.pierre@outlook.fr>
2020-11-02 15:51:04 +0100

    Ajout de fonctionnalité

---
+++ Competences Pierre/connexion.php
@@ -0,0 +1,40 @@
+<?php
+include 'Login.php'
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body class="bg-dark">
+
+    <div class="container bg-light mt-5 border border-primary p-5 rounded">
+        <h1>Connectez-vous</h1>
+        <form method="post">
+            <div class="form-group">
+                <label for="exampleInputEmail2">Nom</label>
+                <input type="text" class="form-control" id="exampleInputEmail2" name="name" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Prenom</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="firstname" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail3">adresse Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail3" name="email" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">mot de passe</label>
+                <input type="password" class="form-control" name="pwd" id="exampleInputPassword1">
+            </div>
+            <input type="submit" class="btn btn-primary" name="submit" value="Connectez-vous">
+        </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Competences Pierre/enregistrement.php
@@ -0,0 +1,23 @@
+<?php
+include 'ConnectionBDD.php';
+try {
+    $bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=UTF8', $user, $pass);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
+$query = $bdd->prepare("INSERT INTO users(name,firstname,email,password) VALUES(:name , :firstname , :email , :password)");
+
+if (isset($_POST["submit"])) {
+    if (isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['firstname']) && !empty($_POST['firstname']) && isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['pwd']) && !empty($_POST['pwd'])) {
+        $query->bindParam(':name', $_POST['name'], PDO::PARAM_STR);
+        $query->bindParam(':firstname', $_POST['firstname'], PDO::PARAM_STR);
+        $query->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
+        $query->bindParam(':password', $_POST['pwd'], PDO::PARAM_STR);
+        $query->execute();
+        header('Location: index.php');
+    } else {
+        echo 'Tout les champs doivent être remplis';
+    }
+}
