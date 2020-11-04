fedfd5ae01d7b57df5a6f5e43dd4834e7f25effa
redlafougere <69895192+redlafougere@users.noreply.github.com>
2020-11-04 16:49:56 +0100

    Update stream.php

--- site perso/stream.php
+++ site perso/stream.php
@@ -14,11 +14,50 @@
 
 <?php
 include "php/php1.php";
-HEAD("Page d'accueil");
+HEAD("Stream");
 ?>
 
 <body >
 
+<nav>
 <?php
 NAVB();
 ?>
+</nav>
+
+    <!-- container -->
+    <div class="container-fluid" >
+
+        <!-- row-->
+        <div class="row p-3">
+
+            <!-- column stream-->
+            <div class="stream col-xs-12 col-sm-12 col-md-12 col-lg-10 bg-dark embed-responsive embed-responsive-16by9 ">
+                <iframe 
+                    src="https://player.twitch.tv/?channel=redlafougere&parent=localhost"
+                    frameborder="0"
+                    allowfullscreen="true"
+                    scrolling="no">
+                </iframe>
+            </div>
+            <!-- column stream-->
+
+            <!-- column chat-->
+            <div class="stream d-none d-xs-none d-sm-none d-md-none d-lg-block col-xs-12 col-sm-12 col-md-12 col-lg-2 bg-dark embed-responsive embed-responsive-16by9">
+                <iframe
+                    id="chat_embed"
+                    src="https://www.twitch.tv/embed/redlafougere/chat?parent=localhost">
+                </iframe>
+            </div>
+            <!-- column chat-->
+
+        </div>
+        <!-- row -->
+
+    </div>
+    <!-- container -->
+
+  <!-- https://www.matthieu-jalbert.fr/integrer-une-chaine-twitch-a-wordpress/ -->
+
+
+<boby>
