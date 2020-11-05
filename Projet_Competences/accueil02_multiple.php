b7a3d62840a06c9933e7ab166dfd8efed1785ae6
redlafougere <69895192+redlafougere@users.noreply.github.com>
2020-11-05 14:18:08 +0100

    début

---
+++ Projet_Competences/accueil02_multiple.php
@@ -0,0 +1,70 @@
+<?php
+require_once( "parametres.php" );
+include_once(  "CO_global_functions.php"  );
+
+function setWidgetValue2( $skill  )
+{
+    $widget = "<p>".$skill[1]."</p><input id='number' type='number' value='0' name='valSkill' min='0' max='10' onchange=\"MAJ_Value( ".$skill[0].", this.value )\">\n";
+    return $widget;
+}
+
+function setWidgetValue( $skill )
+{
+    $widget = "<p>".$skill[1]."</p><input type='range'  value='0' class='form-control-range' min='0' step='1' max='10' id='".$skill[0]."' name='valSkill' onchange=\"MAJ_Value( ".$skill[0].", this.value )\" >\n";
+    return $widget;
+}
+
+
+function setAllWidgetValue( $skills  )
+{
+    $widget = "<div id='valSkills' >\n";
+    foreach( $skills as $skill )
+        $widget .= setWidgetValue2( $skill );
+    $widget .= "</div>\n";
+    return $widget;
+}
+
+
+
+session_start();
+$ID_user = $_SESSION[ 'ID_user' ];
+$name_user = $_SESSION[ 'name' ];
+$surname_user = $_SESSION[ 'surname' ];
+
+echo "<h3>bonjour $surname_user</h3>\n";
+
+
+$req = "SELECT * FROM $DB_dbname.skills";
+$result = executeSQL( $req );
+
+$skills = [];
+while( $data = $result->fetch_assoc())
+{
+    array_push( $skills, [ $data['id'], $data[ 'name']   ] );
+}
+
+//print_r( $skills );
+
+
+?>
+<script>
+    function MAJ_Value( id_skill, value  )
+    {
+      var xhttp = new XMLHttpRequest();
+      
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            document.getElementById("message_validation").innerHTML = "valeur enregistrée";
+        }
+      };
+
+      xhttp.open("GET", "maj_value.php?idSkill="+id_skill+"&valSkill="+value, true);
+      xhttp.send();
+    }   
+</script>
+
+
+<FORM  method='POST' name="formSkill" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<?php echo setAllWidgetValue( $skills ); ?>
+</FORM>
---
+++ Projet_Competences/parametres.php
@@ -0,0 +1,9 @@
+<?php
+
+	$DB_URL 	= "localhost";
+
+	$DB_user 	= "root";
+	$DB_PW 		= "";
+	$DB_dbname 	= "dwwm_maubeuge";
+
+?>
