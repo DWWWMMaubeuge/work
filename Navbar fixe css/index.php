b7c6fac861a5fff0d52d7389310270dbd0414545
Flavia Fortunato <fortu@LAPTOP-J0L2NV6L.fortunatoflavia@outlook.com>
2020-11-06 15:38:49 +0100

    Navbar fixe css 06.11.2020

---
+++ Navbar fixe css/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>  </title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+    <div class="box-area">
+        <header>
+            <div class="wrapper">
+                <div class="logo">
+                    <a href="#"> Flavia </a>
+                </div>
+                <nav>
+                    <a href="#"> A propos de moi </a>
+                    <a href="#"> Curriculum vitae </a>
+                    <a href="#"> Mes créations </a>
+                    <a href="#"> Contact </a>
+                </nav>
+            </div>
+        </header>
+        <div class="banner-area">
+            <h2>voici la banniere</h2>
+        </div>
+        <div class="content-area">
+            <div class="wrapper">
+                <h2> Contenu </h2>
+                <p>paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe
+                 du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu 
+                 paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe du contenu paragraphe 
+                 paragraphe du contenu paragraphe du contenu paragraphe du contenu du contenu 
+                 paragraphe du contenu </p>
+            </div>
+         </div>
+  ...</div>
+</body>
+</html>
---
+++ Navbar fixe css/index1.php
@@ -0,0 +1,8 @@
+<?php
+
+$title = "Home";
+$content = "hello world";
+
+include 'index.php';
+
+?>
