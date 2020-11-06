0bef4e8ff640007dc4383294f653a8b53a937781
nicolas <nicolascaulier@gmail.com>
2020-11-06 12:08:22 +0100

    ok

--- projet_perso/login.php
+++ projet_perso/login.php
@@ -31,7 +31,39 @@ if( $_POST && $_POST['mail'] != "" && $_POST['password'] != ""  )
     echo "<h3>login incorrect</h3>";
 }
 
+?>
 
 // créer le formulaire de connexion
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="login-box">
+    <FORM  method='POST' action="<?=$_SERVER['PHP_SELF']; ?>">
+        <div class="bg-image">
+        </div>
+        
+        <h1>Login</h1>
+        
+        <div  class="textbox">
+             <i class="fas fa-envelope"></i>
+             <INPUT type='text' name='email' placeholder="Your email">
+        </div>
 
+        <div  class="textbox">
+            <i class="fa fa-lock" aria-hidden="true"></i>
+            <input type="password" placeholder="Password" name="password" value="">
+        </div>
+        
+        <input class="btn" type="submit" name="btn" value="Log in">
+    </FORM>
+</div>
+</body>
+</html>
