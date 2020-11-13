3d912c431e7048f3fd60ac3a94493103e5a0905f
Pierre <delattre.pierre@outlook.fr>
2020-11-13 16:11:57 +0100

    Captcha 3.0

--- Captcha/captcha.php
+++ Captcha/captcha.php
@@ -1,15 +1,25 @@
 <?php
 session_start();
  
-$_SESSION['captcha'] = mt_rand(1000,9999);
-$img = imagecreate(65,30);
-$font = 'fonts/destroyfont.ttf';
+$_SESSION['captcha'] = rand(1000,9999);
+$img = imagecreatetruecolor(70,30);
+$fill_color = imagecolorallocate ($img,255,255,255);
+imagefilledrectangle ($img, 0, 0,70,30,$fill_color);
+$text_color = imagecolorallocate($img, 10,10,10);
+$font = "./police.ttf";
+imagettftext($img,23,0,5,30,$text_color,$font,$_SESSION['captcha']);
+header('Content-type: image/jpeg');
+imagejpeg($img);
+imagedestroy($img);
+/*$font = '28DaysLater.ttf';
  
 $bg = imagecolorallocate($img,255,255,255);
 $textcolor = imagecolorallocate($img, 0, 0, 0);
  
-imagettftext($img, 23, 0, 3, 30, $textcolor, $font, $_SESSION['captcha']);
- 
 
+ 
+header('Content-type: image/jpeg');
+imagejpeg($img);
+imagedestroy($img);*/
  
 ?>
