14b116fc183211b595ae84075669ac9a240da187
Maxime Wilmot <you@example.com>
2020-11-16 15:56:48 +0100

    fin de journee 16novembre 2020

---
+++ Projet perso/projet_papa.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href= "projet_papaCSS.css">
+</head>
+<body>
+    <h1 >Formulaire d'inscription</h1>
+    
+    
+    
+</body>
+</html>
+<?php
+
+if ( isset($_POST) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['age']) && isset($_POST['email']) && isset($_POST['password']))
+{
+	$adresse 		= "localhost";
+	$utilisateur 	= "root";
+	$password 		= "";
+
+	$connexion = new mysqli( $adresse, $utilisateur, $password );
+
+	if ( $connexion->connect_error == true )
+	 	echo "une erreur de connexion s'est produite<br>";
+	else
+	 	echo "connexion réussie<br>";
+
+	$requete 	= "insert into projet.users (nom, prenom, age, email, password ) values ( '".$_POST['nom']."', '".$_POST['prenom']."', ".$_POST['age'].", ".$_POST['email'].", ".$_POST['password'].");";
+
+
+	//echo "$requete<br>";
+
+	$connexion->query( $requete );
+
+	$connexion->close();
+}
+?>
+
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+     
+     <h2>Nom</h2>
+     <input type="text"  	name="nom" 		placeholder="nom ">
+<br>
+  <h2>Prénom</h2>
+  <input type="text"  	name="prenom" 	placeholder="prenom">
+<br>
+  <h2>Age</h2>
+  <input type="number"  	name="age" 		placeholder="age">
+<br>
+  <h2>E-Mail</h2>
+  <input type="text"  	name="email" 	placeholder="email">
+<br>
+  <h2>Mot De Passe</h2>
+  <input type="text"  	name="password" 	placeholder="password">
+<br>
+<input type="submit"  	name="OK" 		value="OK">
+</form>
+	
+	
+	
+  
