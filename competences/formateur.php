40c6c86b7b89c58c0f5dc243a08133540bf7ad37
steven <jeffrey_stl@hotmail.fr>
2020-11-23 16:17:55 +0100

    save

--- competences/formateur.php
+++ competences/formateur.php
@@ -2,18 +2,9 @@
 require_once( "parametres.php" );
 include_once(  "CO_global_functions.php"  );
 
-<<<<<<< HEAD
-//<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-/*<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>*/
-=======
 // ajouter des skills
 // voir les resultats
 // ajouter des stagiaires
-
->>>>>>> main
 
 session_start();
 
@@ -25,6 +16,7 @@ $ID_formation   = $_SESSION[ 'ID_formation' ];
 $ID_user        = $_SESSION[ 'ID_user' ];
 $name_user      = $_SESSION[ 'name' ];
 $surname_user   = $_SESSION[ 'surname' ];
+$ID_sessions	= $_SESSION[ 'ID_session' ];
 
 echo "<h3>bonjour $surname_user</h3>\n";
 
@@ -37,17 +29,16 @@ echo "<h3>formation : $formationName</h3>\n";
 
 // XXXXX
 // recupère le nom de la session si il en a une
-$req = "SELECT * FROM $DB_dbname.sessions .......... ";
-//$result = executeSQL( $req );
-//$data = $result->fetch_assoc();
-//$sessionsName = $data[ 'name'];
-//echo "<h3>session : $sessionsName</h3>\n";
+$req = "SELECT * FROM $DB_dbname.sessions where id=$ID_sessions";
+$result = executeSQL( $req );
+$data = $result->fetch_assoc();
+$sessionsName = $data[ 'name'];
+echo "<h3>session : $sessionsName</h3>\n";
 
 
 // XXXXX
 // recupère la liste des stagiaires
-/*
-$req = "SELECT * FROM $DB_dbname.users ............";
+$req = "SELECT * FROM $DB_dbname.users where id=$ID_users";
 $res = executeSQL( $req );
 $list_stagiaires = "<ul name=\"listStagiare\" >\n";
 while( $ligne = $res->fetch_assoc() )
@@ -56,7 +47,7 @@ while( $ligne = $res->fetch_assoc() )
 	$list_stagiaires .="<li>".$ligne['name']."</li>\n";
 }
 $list_stagiaires .= "</ul>\n";
-*/
+
 
 
 ?>
