b41357a80674e3ef5558beb6c86459d47a52575a
steven <jeffrey_stl@hotmail.fr>
2020-11-23 11:57:44 +0100

    save

--- competences/admin.php
+++ competences/admin.php
@@ -2,18 +2,12 @@
 require_once( "parametres.php" );
 include_once(  "CO_global_functions.php"  );
 
-
-/*<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>*/
-
 session_start();
 
 // verifier que le visteurs est bien passé par le login (donc il a un ID_user != 0)
 // verifier que le visteurs est bien administrateur (donc il a un role ==   ADM )
 if ( !isset( $_SESSION[ 'ID_user' ]) || $_SESSION[ 'ID_user' ] == 0 || $_SESSION[ 'role' ] != 'ADM')
     header( "location: login01.php");
-
 
 // recupération des formations pour le comboBox
 
@@ -41,7 +35,7 @@ if( $_POST && isset($_POST['name']) && $_POST['surname'] != "" && $_POST['mail']
     $mail       = $_POST['mail'];
     $password   = $_POST['password'];
 
-    // attention aux doublons des mail
+    // attention aux doublons des mails
 
     $req = "INSERT INTO $DB_dbname.users ( name, surname, mail, password ) VALUES ( '$name', '$surname', '$mail', '$password' )";
     executeSQL( $req );
