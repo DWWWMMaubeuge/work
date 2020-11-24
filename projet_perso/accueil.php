530a3af767199898a7be441826daf9f91d72a828
nicolas <nicolascaulier@gmail.com>
2020-11-24 16:09:03 +0100

    24/11/2020

--- projet_perso/accueil.php
+++ projet_perso/accueil.php
@@ -3,7 +3,7 @@ session_start();
 $id=0;
 if(isset($_SESSION[ 'ID_user' ]))
     $id=$_SESSION[ 'ID_user' ];
-    $name= $_SESSION['name'];
+    //$name= $_SESSION['name'];
 include "header.php";
 echo entete();
 include "navbar.php";
@@ -21,8 +21,15 @@ echo nav($id);
 <div class="titre">
 <h1>Nicolas CAULIER Développeur Web</h1>
 <?php
+
 if(isset($_SESSION[ 'ID_user' ]))
+{
+    $name= $_SESSION['name'];
 echo "<h2 class=\"lol\">bienvenue $name</h2>";
+}
+else{
+    
+}
 ?>
 </div>
 
