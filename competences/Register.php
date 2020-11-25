ee46443dc370b8ac8445733ba3225b1599e59ff2
iPuSheR <ahanchir@live.fr>
2020-11-25 16:45:34 +0100

    MaJ

--- competences/Register.php
+++ competences/Register.php
@@ -41,7 +41,104 @@ if( $_POST && isset($_POST['name']) && $_POST['surname'] != "" && $_POST['mail']
     header( "location: Login.php");
 }
  
+
+
+
+if (isset($_POST['mail'])) {
+
+// EDIT THE 2 LINES BELOW AS REQUIRED
+$mail= "devafpa2020@gmail.com";
+//$mail_to = "@live.fr";
+$mail_subject = "RECLAMATION";
+
+function died($error)
+{
+    // your error code can go here
+    echo
+        "Nous sommes désolés, mais des erreurs ont été détectées dans le" .
+        " formulaire que vous avez envoyé. ";
+    echo "Ces erreurs apparaissent ci-dessous.<br /><br />";
+    echo $error . "<br /><br />";
+    echo "Merci de corriger ces erreurs.<br /><br />";
+    die();
+}
+
+ // si la validation des données attendues existe
+if (!isset($_POST['name']) ||
+    !isset($_POST['surname']) ||
+    !isset($_POST['mail']) ||
+    !isset($_POST['telephone']) ||
+    !isset($_POST['commentaire'])) {
+    died(
+        'Nous sommes désolés, mais le formulaire que vous avez soumis semble poser' .
+        ' problème.');
+} 
+
+$name = $_POST['name']; // required
+$surname = $_POST['surname']; // required
+$mail = $_POST['mail']; // required
+$telephone = $_POST['telephone']; // not required
+$commentaire = $_POST['commentaire']; // required
+
+$error_message = "";
+$mail_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+if (!preg_match($mail_exp, $mail)) {
+    $error_message .=
+        'L\'adresse e-mail que vous avez entrée ne semble pas être valide.<br />';
+}
+
+// Prend les caractères alphanumériques + le point et le tiret 6
+$string_exp = "/^[A-Za-z0-9 .'-]+$/";
+
+if (!preg_match($string_exp, $name)) {
+    $error_message .=
+        'Le name que vous avez entré ne semble pas être valide.<br />';
+}
+
+if (!preg_match($string_exp, $surname)) {
+    $error_message .=
+        'Le préname que vous avez entré ne semble pas être valide.<br />';
+}
+
+if (strlen($commentaire) < 2) {
+    $error_message .=
+        'Le commentaire que vous avez entré ne semble pas être valide.<br />';
+}
+
+if (strlen($error_message) > 0) {
+    died($error_message);
+}
+
+$mail_message = "Détail.\n\n";
+$mail_message .= "name: " . $name . "\n";
+$mail_message .= "surname: " . $surname . "\n";
+$mail_message .= "mail: " . $mail . "\n";
+$mail_message .= "Telephone: " . $telephone . "\n";
+$mail_message .= "Commentaire: " . $commentaire . "\n";
+
+// create mail headers
+$headers = 'From: ' . $mail . "\r\n" .
+'Reply-To: ' . $mail . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($mail, $mail_subject, $mail_message, $headers);
+
+
 ?>
+
+
+
+<p style=text-align:center>Notre service client est à votre écoute pour vous conseiller. Laissez-nous vos coordonnées, nous vous contacterons très vite..</p>
+
+<!-- Merci de nous avoir contacter. Nous vous contacterons très bientôt. -->
+
+
+<?php
+
+}
+
+?>
+
 <!DOCTYPE html>
     <html lang="fr">  
     <head>
@@ -60,11 +157,11 @@ if( $_POST && isset($_POST['name']) && $_POST['surname'] != "" && $_POST['mail']
             <FORM  method='POST' action="<?php echo $_SERVER['PHP_SELF']; ?>">
                 <img src="img/logo6.png" alt=""> 
                 <label for=""><h2>INSCRIPTION</h2></label>
-                <INPUT type='text' name='name' placeholder="votre nom ici">
+                <INPUT type='text' name='name' placeholder="votre name ici">
                 <br>
-                <INPUT type='text' name='surname' placeholder="votre prenom">
+                <INPUT type='text' name='surname' placeholder="votre surname">
                 <br>
-                <INPUT type='email' name='mail' placeholder="votre mail">
+                <INPUT type='mail' name='mail' placeholder="votre mail">
                 <br>
                 <INPUT type='text' name='password' placeholder="votre mot de passe">
                 <br>
