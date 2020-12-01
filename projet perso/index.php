0cd98d758c56c1deea07215ea92dbd5c5f0d26a6
Flavia Fortunato <fortu@LAPTOP-J0L2NV6L.fortunatoflavia@outlook.com>
2020-12-01 16:31:49 +0100

    debut d'un projet perso

---
+++ projet perso/index.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+        echo "<!DOCTYPE html>\n";
+        echo "<html>\n";
+        echo "<head>\n";
+        echo "<meta charset=\"utf-8\">\n";
+        
+        echo "<link rel=\"stylesheet\" href=\"background_styles.css\" />\n";
+        echo "<link rel=\"stylesheet\" href=\"style.css\">\n";
+        echo "<script src=\"script.js\" defer\">\n";
+        echo "<title> Nav bar </title>\n";
+        echo "</script>\n";
+        echo "</head>\n";
+        echo "<body>\n";
+        echo "<nav class=\"navbar\">\n";
+        echo "<div class=\"brand-title\"> Site </div>\n";
+        echo "<a href=\"#\"class=\"toggle-button\">\n";
+        echo "<span class=\"bar\">\n"; 
+        echo "</span>\n";
+        echo "<span class=\"bar\">\n"; 
+        echo "</span>\n";
+        echo "<span class=\"bar\">\n"; 
+        echo "</span>\n";
+        echo "</a>\n";
+        echo "<div class=\"navbar-links\">\n";
+        echo "<ul>\n";
+
+        echo "<li>\n";
+        echo "<a href=\"#\"<i class=\"fas fa-home\"></i>Home</a>\n";
+        echo "</li>\n";
+        echo "<li>\n";
+        echo "<a href=\"#\"<i class=\"fas fa-home\"></i>About</a>\n";
+        echo "</li>\n";
+
+        echo "<li>\n";
+        echo "<a href=\"#\"<i class=\"fas fa-home\"></i>Contact</a>\n";
+        echo "</li>\n";
+
+        echo "</ul>\n";
+        echo "</div>\n";
+        echo "</nav>\n";
+        echo "</body>\n";
+        echo "</html>\n";
+
+        ?>
