61c3f577cff6fcb3623debe9df703969f97301c4
name <valentin.crapez1995@gmail.com>
2020-12-08 17:06:03 +0100

    tuto MERN

---
+++ MERN/client/src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import {useState,useEffect} from "react"
+import Axios from "axios"
+
+
+function App() {
+
+  const [name,setName]= useState("")
+  const [age,setAge]= useState(0)
+  const [listOfFriends, setListOfFriends] = useState([])
+
+  const addFriend = ()=>{
+    Axios.post('http://localhost:3001/addfriend',{
+      name: name,
+      age: age
+      }).then((response)=>{
+        setListOfFriends([...listOfFriends, {_id:response.data._id, name:name, age:age}])
+      })
+    }
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/read').then((response)=>{
+        setListOfFriends(response.data)
+        //console.log(response.data)
+    }).catch(()=> {
+      console.log('error')
+    })
+  },[])
+
+  const updateFriend = (id) => {
+    const newAge= prompt("Enter new age")
+    Axios.put('http://localhost:3001/update',{newAge: newAge, id: id}).then(()=>{
+      setListOfFriends(listOfFriends.map((val)=>{
+        return val._id === id ? {_id: id, name: val.name, age: newAge} : val
+      }))
+    })
+  }
+
+  const deleteFriend = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then(()=>{
+      setListOfFriends(listOfFriends.filter((val)=>{
+        return val._id !== id
+      }))
+    })
+  }
+
+  return (
+    <div className="App">
+      <div className="inputs">
+        <input type="text" placeholder="friend name..." onChange={(e)=>setName(e.target.value)}/>
+        <input type="number"  placeholder="friend age..." onChange={(e)=>setAge(e.target.value)}/>
+
+        <button onClick={addFriend}>Add Friend</button>
+      </div>
+      <div className="listOfFriends">
+        {listOfFriends.map((val)=> {
+          return (
+          <div className="friendContainer" key={val._id}>
+            <div className="friend" >
+              <h3>Name: {val.name}</h3>
+              <h3>Age: {val.age}</h3>
+            </div>
+            <div>
+              <button onClick={() => updateFriend(val._id)}>Update</button>
+              <button id="removeBtn" onClick={() => deleteFriend(val._id)}>X</button>
+            </div>
+          </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
