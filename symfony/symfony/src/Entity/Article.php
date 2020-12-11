f95530a0498955618e2085cd7de4506c65be898b
Flavia Fortunato <fortu@LAPTOP-J0L2NV6L.fortunatoflavia@outlook.com>
2020-12-11 15:26:00 +0100

    test connexion bdd symfony 11.12.2020

---
+++ symfony/symfony/src/Entity/Article.php
@@ -0,0 +1,61 @@
+<?php
+// src/Entity/Article.php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    //private $id;
+
+    // ...
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Category")
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    // ...
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->contains($category)) {
+            $this->categories->removeElement($category);
+        }
+
+        return $this;
+    }
+}
