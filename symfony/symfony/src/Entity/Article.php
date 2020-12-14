1596a42bc072d9d3bf9760521905fc8079a8b917
Flavia Fortunato <fortu@LAPTOP-J0L2NV6L.fortunatoflavia@outlook.com>
2020-12-14 16:35:33 +0100

    14.12.2020

--- symfony/symfony/src/Entity/Article.php
+++ symfony/symfony/src/Entity/Article.php
@@ -1,5 +1,4 @@
 <?php
-// src/Entity/Article.php
 
 namespace App\Entity;
 
@@ -17,12 +16,40 @@ class Article
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    //private $id;
-
-    // ...
+    private $id;
 
     /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Category")
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publicationDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastUpdateDate;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Category", inversedBy="articles")
      */
     private $categories;
 
@@ -31,31 +58,10 @@ class Article
         $this->categories = new ArrayCollection();
     }
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
     // ...
-
-    /**
-     * @return Collection|Category[]
-     */
-    public function getCategories(): Collection
-    {
-        return $this->categories;
-    }
-
-    public function addCategory(Category $category): self
-    {
-        if (!$this->categories->contains($category)) {
-            $this->categories[] = $category;
-        }
-
-        return $this;
-    }
-
-    public function removeCategory(Category $category): self
-    {
-        if ($this->categories->contains($category)) {
-            $this->categories->removeElement($category);
-        }
-
-        return $this;
-    }
 }
