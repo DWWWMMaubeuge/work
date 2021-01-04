6c0b123b5bcf26b171881f0321a3217474ad0963
Steven Durieux <stevenhonor@live.fr>
2021-01-04 17:34:07 +0100

    Ajout de documentation

--- ping-pong/pingpong.js
+++ ping-pong/pingpong.js
@@ -9,7 +9,7 @@ verticalUnit = "vh";
 horizontalUnit = "vw";
 score = 0;
 record = 0;
-cash = 0;
+cash = 15000;
 scoreNumber.innerHTML = score;
 recordNumber.innerHTML = record;
 cashNumber.innerHTML = cash;
@@ -28,15 +28,20 @@ notEnoughCashSound = new Audio('assets/soundeffects/notenoughcash.wav');
 
 // Création du bouton Start
 function createStartButton() {
+    // Creation du container du bouton
     startContainer = document.createElement('DIV');
     startContainer.id = "startContainer";
-    startButton = document.createElement('button');
+    // Création du bouton start
+    startButton = document.createElement('BUTTON');
     startButton.id = "startButton";
     startButton.innerHTML = "Démarrer le jeu";
     startButton.onclick = "startGame()";
+    // Insertion du bouton dans son container
     startContainer.appendChild(startButton);
+    // Insertion du container dans le jeu
     game.appendChild(startContainer);
-    startButton.onclick = function() {startGame();}
+    // Attribution du onclick au bouton
+    startButton.onclick = function() { startGame(); }
 }
 
 // Création de la raquette et ajout d'un écouteur sur la page pour les mouvements
@@ -46,23 +51,35 @@ function createRaquette(asset) {
     raquette.id = "raquette";
     raquetteLeftPos = 34;
     raquette.style.top = "50" + verticalUnit;
+    // On définit la position initiale de la raquette pour la centrer
     raquette.style.left = raquetteLeftPos + horizontalUnit;
+    // Insertion de la raquette dans le jeu
     game.appendChild(raquette);
+    // On ajoute l'écouteur à la fenetre que si le container du bouton start existe
     if(typeof(startContainer) !== "undefined") {
         body.addEventListener('keydown', e=>raquetteMouvements(e));
     }
 }
 
 function raquetteMouvements(e) {
+    // On récupére le code de la touche pressée
     keyPressed = e.key;
+    // Si la touche pressé correspond à la fleche droite
     if(keyPressed == "ArrowRight") {
+        // Si la nouvelle position de la raquette ne dépasse pas la bordure droite du jeu
         if((raquetteLeftPos +3) <= 67) {
+            // On change la position de la raquette 
             raquetteLeftPos = raquetteLeftPos + 3;
+            // On fais bouger l'image de la raquette
             raquette.style.left = raquetteLeftPos + horizontalUnit;
         }
+    // Si la touche pressé correspond à la fleche gauche
     } else if(keyPressed == "ArrowLeft") {
+        // Si la nouvelle position de la raquette ne dépasse pas la bordure gauche du jeu
         if((raquetteLeftPos -3) >= 0) {
+            // On change la position de la raquette 
             raquetteLeftPos = raquetteLeftPos - 3;
+            // On fais bouger l'image de la raquette
             raquette.style.left = raquetteLeftPos + horizontalUnit;
         }
     }
@@ -75,8 +92,10 @@ function createBall() {
     balle.id = "balle";
     balleLeftPos = 29;
     balleTopPos = 27;
+    // On définit la position initiale de la balle pour la centrer
     balle.style.left = balleLeftPos + horizontalUnit;
     balle.style.top = balleTopPos + verticalUnit;
+    // Insertion de la balle dans le jeu
     game.appendChild(balle);
 }
 
@@ -88,7 +107,7 @@ function ballMovements() {
     // console.log('position raquette: ' + raquetteLeftPos);
     // console.log('hauteur balle: ' + balleTopPos);
     // console.log('indice collision: ' + collision);
-    // collision fenetre à gauche et à droite
+    // collision fenetre gauche et à droite
     if(leftAngle == true && balleLeftPos == parseFloat(-7)) {
         leftAngle = false;
         rightAngle = true;
@@ -161,7 +180,7 @@ function gameOver() {
     // On joue l'effet sonore du game over
     gameOverSound.play();
     // On crée un écran de game over dans une div qui contiendra un bouton pour relancer le jeu
-    gameOverScreen = document.createElement('div');
+    gameOverScreen = document.createElement('DIV');
     gameOverContent = "Vous avez perdu !<br><br><button onclick='startGame()'>Cliquez ici pour reessayer</button>";
     gameOverScreen.innerHTML = gameOverContent;
     gameOverScreen.id = "gameOver";
@@ -181,7 +200,7 @@ function checkBallOut() {
     }
 }
 
-// Fonction d'ouverture/fermeture du shop
+// Ouverture/fermeture du shop
 function openShop() {
     // Si lorsque l'utilisateur clique sur le bouton shop et qu'il est ouvert:
     if(shop.style.display == "block") {
@@ -205,31 +224,48 @@ function openShop() {
     }
 }
 
+// Achat d'un item
 function buyItem(price, buttonId, priceSpanId) {
+    // Si le prix est inférieur ou égal au montant d'argent actuel du joueur
     if(price <= cash) {
+        // On déduit le prix de l'argent du joueur
         cash = cash - price;
+        // On affiche le nouveau montant d'argent
         cashNumber.innerHTML = cash;
+        // Si le bruitage d'achat est déjà en cours de lecture on l'arrête
         if(itemPurcharsedSound.paused != true) {
             itemPurcharsedSound.currentTime = 0;
         }
+        // On joue le bruitage d'achat
         itemPurcharsedSound.play();
+        // Suppression du span contenant le prix de l'item
         document.getElementById(priceSpanId).style.display = "none";
+        // Remplacement du contenu du bouton pour le faire passer de "acheter" à "équiper"
         document.getElementById(buttonId).innerHTML = "Equiper";
+        // Changement du onclick du bouton pour lui attraper la fonction equipItem avec en parametre l'id du bouton
         document.getElementById(buttonId).onclick = function() { equipItem(buttonId)}
+    // Sinon on joue l'effet sonore qui signale au joueur qu'il n'a pas assez d'argent
     } else {
+        // Si l'effet sonore est déjà en cours de lecture on l'arrête
         if(notEnoughCashSound.paused != true) {
             notEnoughCashSound.currentTime = 0;
         }
+        // On joue l'effet sonore
         notEnoughCashSound.play();
     }
 }
 
+// Changement de raquette par le joueur
 function equipItem(asset) {
+    // Changement de la variable de l'image de la raquette avec celle que l'utilisateur a équiper
     raquetteImage = asset;
+    // Si le bruitage est déjà en cours de lecture alors on l'arrête
     if(itemEquippedSound.paused != true) {
         itemEquippedSound.currentTime = 0;
     }
+    // Lecture du bruitage
     itemEquippedSound.play();
+    // Si le jeu est déjà lancé, on remplace directement l'image de la raquette
     if(typeof(startButton) == "undefined") {
         raquette.src = "";
         raquette.src = "assets/raquettes/"+asset+".png";
@@ -247,6 +283,7 @@ function startGame() {
     if(typeof(startContainer) !== "undefined") {
         game.removeChild(startContainer);
         delete startContainer;
+        delete startButton;
     } else {
         // remise à zéro du score lors du relancement d'une partie
         score = 0;
