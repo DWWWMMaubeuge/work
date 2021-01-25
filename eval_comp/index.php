d5dc85e7a91a2710756ca0751547e9e91a58d6d7
Steven Durieux <stevenhonor@live.fr>
2021-01-25 11:44:28 +0100

    mise à jours des projets

--- eval_comp/index.php
+++ eval_comp/index.php
@@ -6,10 +6,7 @@
 <div class="container-fluid banner mt-5 p-5" id="top">
     <div class="row">
         <div class="col-md-8 offset-md-2 info">
-            <h1 class="text-center">DWM Rousies</h1>
-            <p class="text-center">
-                Bienvenue sur le site des développeurs full-stack de Maubeuge.
-            </p>
+            <h1 class="text-center my-5">Bienvenue sur le site d'auto-évaluation de l'AFPA.</h1>
             <button class="btn btn-md text-center" id="myButton" onclick="showDescription()">En savoir plus</button>
         </div>
     </div>
@@ -19,12 +16,12 @@
     <div class="row">
         <div class="col-md-8 col-sm-8 text-center offset-md-2 offset-sm-2 bg-dark opacity-4 rounded info2">
             <div class="my-5 my-5-sm">
-                <h2 id="description">Qui sommes nous ?</h2>
-                <p>Nous sommes les apprenants de la formation développeur web de l'AFPA de Rousies et nous sommes actuellement en train de passer notre titre professionnel de développeur web.</p>
+                <h2 id="description">Qui suis-je ?</h2>
+                <p>je suis un apprenant de la formation développeur web de l'AFPA de Rousies et je suis actuellement en train de passer mon titre professionnel de développeur web.</p>
             </div>
             <div class="my-5">
                 <h2>Quel est le but de ce site ?</h2>
-                <p>L'objectif principal de ce projet était de créer une plateforme où chacun peut évaluer ses compétences par rapport à sa formation. Les formateurs peuvent modifier leur formation à leur guise, en ajoutant ou supprimant des compétences par exemple. Mais ce site a également un second rôle qui est de montrer ce dont nous sommes capables lors de la réalisation de projet.</p>
+                <p>L'objectif principal de ce projet était de créer une plateforme où chacun peut évaluer ses compétences par rapport à sa formation. Les formateurs peuvent modifier leur formation à leur guise, en ajoutant ou supprimant des compétences par exemple. Mais ce site a également un second rôle qui est de montrer ce dont je suis capable lors de la réalisation de projet.</p>
             </div>
             <div class="my-5">
                 <button class="btn btn-danger btn-md text-center" id="myButton" onclick="closeDescription()">Fermer</button>
