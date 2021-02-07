a9f72b4e08a5daceaeb8e40ba7812e82d870b9d1
Steven Durieux <stevenhonor@live.fr>
2021-02-08 00:21:10 +0100

    mise a jour projet

--- stage_afpa/required/sidebar.php
+++ stage_afpa/required/sidebar.php
@@ -19,10 +19,10 @@
         <img src="https://dwm-competences.000webhostapp.com/images/avatars/21.gif" class="profile_image" alt="Photo de profil">
         <h4>Steven</h4>
     </div>
-    <a onclick="showContent('dashboard', true);" href="#tableaudebord"><i class="fas fa-desktop"></i><span>Tableau de bord</span></a>
-    <a onclick="showContent('monespace', true);" href="#monespace"><i class="fas fa-user-circle"></i><span>Espace personnel</span></a>
-    <a onclick="showContent('formateur', true);" href="#formateur"><i class="fas fa-chalkboard-teacher"></i><span>Espace formateur</span></a>
-    <a onclick="showContent('redacteur', true);" href="#redacteur"><i class="fas fa-pen"></i><span>Espace rédacteur</span></a>
-    <a onclick="showContent('parametres', true);" href="#parametres"><i class="fas fa-sliders-h"></i><span>Paramètres</span></a>
+    <a onclick="showContent('dashboard', false);" href="#tableaudebord"><i class="fas fa-desktop"></i><span>Tableau de bord</span></a>
+    <a onclick="showContent('monespace', false);" href="#monespace"><i class="fas fa-user-circle"></i><span>Espace personnel</span></a>
+    <a onclick="showContent('formateur', false);" href="#formateur"><i class="fas fa-chalkboard-teacher"></i><span>Espace formateur</span></a>
+    <a onclick="showContent('redacteur', false);" href="#redacteur"><i class="fas fa-pen"></i><span>Espace rédacteur</span></a>
+    <a onclick="showContent('parametres', false);" href="#parametres"><i class="fas fa-sliders-h"></i><span>Paramètres</span></a>
 </div>
 <!--sidebar end-->
